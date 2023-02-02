c80cfff4abde65674803e9b78a0cc516daf41046
Jaeyoung <dlawodud96@naver.com>
2023-02-02 15:19:57 +0900

    ✅ add Custom  App #1 @Imjaae

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,33 +1,23 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
-import styles from "./NavBar.module.css";
 
 export default function NavBar() {
   const router = useRouter();
 
   return (
     <nav>
-      <p>
-        <Link
-          href="/"
-          className={`${styles.link} ${
-            router.pathname === "/" ? styles.active : ""
-          }}`}
-        >
-          Home
-        </Link>
-      </p>
-      <p>
-        <Link
-          href="/about"
-          className={[
-            styles.link,
-            router.pathname === "/about" ? styles.active : "",
-          ]}
-        >
-          About
-        </Link>
-      </p>
+      <Link href="/" legacyBehavior>
+        <a>Home</a>
+      </Link>
+      <Link href="/about" legacyBehavior>
+        <a>About</a>
+      </Link>
+      <style jsx>{`
+        a {
+          color: tomato;
+          padding: 10px;
+        }
+      `}</style>
     </nav>
   );
 }
